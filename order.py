5bb3e95c8666041edd86608d8e02bef908485760
kitkate <kitkate>
2024-02-25 22:06:16 +0000

    Get top 3 most expensive cars natively

--- order.py
+++ order.py
@@ -1,17 +1,11 @@
-from pyspark.sql.functions import col, filter
+from pyspark.sql.functions import col, filter, row_number
+from pyspark.sql.window import Window
 from pyspark.sql.types import DoubleType
 
-def order_cars_by_price_desc_per_make_model(df):
+def top_3_cars_by_price_desc_per_make_model(df):
     df_prices = df.select("make", "model", "version", col("price.consumerValue.gross").alias("price"))
     df_prices_cleaned = df_prices.withColumn("price", df_prices["price"].cast(DoubleType()))
 
-    df_cars_by_price_list = []
-    for make in df_prices_cleaned.select("make").distinct().collect():
-        df_make = df_prices_cleaned.filter(df_prices_cleaned.make == make["make"])
-        for model in df_make.select("model").distinct().collect():
-            if model["model"] is None:
-                df_make_model = df_make.where(col("model").isNull())
-            else:
-                df_make_model = df_make.filter(df_make.model == model["model"])
-            df_cars_by_price_list.append(df_make_model.sort(df_make_model.price.desc()))
-    return df_cars_by_price_list
+    windowDept = Window.partitionBy("make", "model").orderBy(col("price").desc())
+    return df_prices_cleaned.withColumn("row",row_number().over(windowDept)) \
+                            .filter(col("row") <= 3)
--- test_order.py
+++ test_order.py
@@ -3,7 +3,7 @@ import order
 
 class TestOrder(unittest.TestCase):
 
-    def test_order_cars_by_price_desc_per_make_model(self):
+    def test_top_3_cars_by_price_desc_per_make_model(self):
         data = [(("____BLACK", "48573"), 1, "VW", "Arteon", (("2.00", "2"), "BGN"), "1"),
                 (("____WHITE", "48573"), 2, "VW", "Arteon", (("100.00", "2"), "BGN"), "2"),
                 (("____BLUE", "48573"), 3, "VW", "Arteon", (("3.00", "2"), "BGN"), "3")]
@@ -11,10 +11,10 @@ class TestOrder(unittest.TestCase):
         df = spark.createDataFrame(data, schema=schema)
         
         
-        df_ordered_list = order.order_cars_by_price_desc_per_make_model(df)
-        self.assertEqual(df_ordered_list[0].collect()[0]["price"], 100.0, "Cars not ordered correctly by price")
-        self.assertEqual(df_ordered_list[0].collect()[1]["price"], 3.0, "Cars not ordered correctly by price")
-        self.assertEqual(df_ordered_list[0].collect()[2]["price"], 2.0, "ACars not ordered correctly by price")
+        df_ordered = order.top_3_cars_by_price_desc_per_make_model(df)
+        self.assertEqual(df_ordered.collect()[0]["price"], 100.0, "Cars not ordered correctly by price")
+        self.assertEqual(df_ordered.collect()[1]["price"], 3.0, "Cars not ordered correctly by price")
+        self.assertEqual(df_ordered.collect()[2]["price"], 2.0, "ACars not ordered correctly by price")
 
 
 if __name__ == "__main__":
